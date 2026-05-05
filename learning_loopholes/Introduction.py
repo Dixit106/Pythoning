80e8390323ef53fa99091e303c442b46cccabe62
Dixit106 <dixitk2958@gmail.com>
2026-05-05 11:54:23 +0530

    Introduction learning

---
+++ learning_loopholes/Introduction.py
@@ -0,0 +1,23 @@
+# Introduction
+
+print("Helo Em Larning HEHE") #we all know used to print stuff
+
+print(2+3) #addition
+print(3-1) #subraction
+print(2*3) #multiply
+print(3/2) #division
+print(3**2) # means 3 to the power 2
+print(3%2) # modulus(gives remainder in this case it's 1)
+print(3 // 2) # floor division operator(chops off the decimal answer should have been 1.5 but gave 1 in this case)
+
+
+#Checking data types
+
+print(type(10)) #INT
+print(type(3.14)) #Float
+print(type(1+3j)) #Complex
+print(type("abcdEfghijk")) #String
+print(type([1, 2, 3])) #List
+print(type({"name": "Abcdefghi"})) #Dictionary
+print(type((9.8, 3.14, 2.7)))
+print(type({9.8, 3.14, 2.7})) #set 
