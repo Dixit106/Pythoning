aa98532162b04192b01d213e07a3196ab4f6ffee
Dixit106 <dixitk2958@gmail.com>
2026-05-08 12:12:21 +0530

    Learning about Variables

---
+++ learning_loopholes/Variables.py
@@ -0,0 +1,43 @@
+#Variables in python
+
+first_name = 'Luffy'
+middle_name = 'D'
+last_name = 'Monkey'
+country = 'Brazil'
+city = 'Goa Kingdom'
+age = '19'
+is_married = False
+skills = ['Fighting', 'Eating a Lot', 'Dumb and Funny']
+person_info = {
+    'first_name': 'Luffy',
+    'middle_name': 'D',
+    'last_name': 'Monkey',
+    'city': 'Goa Kingdom'
+}
+
+#To print values stored in variable
+
+print('First name: ', first_name)
+print('First name length: ', len(first_name))
+print('Middle name: ', middle_name)
+print('Middle name length: ', len(middle_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+#Declaring multiple variables in one line
+
+First_name, Last_name, Country, Age, Is_married = 'Zoro', 'Ronoroa', 'Unknown', '20', False
+
+print(First_name, Last_name, Country, Age, Is_married)
+print('First name: ', First_name)
+print('Last name: ', Last_name)
+print('Country: ', Country)
+print('Age: ', Age)
+print('Married: ', Is_married)
+
