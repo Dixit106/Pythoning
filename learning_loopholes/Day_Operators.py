662f8e0f2cc7915bcc1b696ca91215f0e9b66e00
Dixit106 <dixitk2958@gmail.com>
2026-05-08 16:41:35 +0530

    Operator's and adapting project based learning

---
+++ learning_loopholes/Day_Operators.py
@@ -0,0 +1,81 @@
+##Arithmetic Operations in Python
+##Integers
+#
+#print("Addition: ", 1+2)
+#print("Subtraction: ", 99-1)
+#print("Multiplication: ", 9*9)
+##Division will give floating numbers
+#print("Division: ", 4/2)
+#print("Division: ", 6/2)
+#print("Division: ", 7/2)
+##Without floating number or without the remaining
+#print("Division without the remainder: ", 7//2)
+#print("Modulus: ", 3%2)
+#print("Division without the remainder: ", 7//3)
+#print("Exponential: ", 3**2)
+#
+##Floating numbers
+#print("Floating Number,PI", 3.14)
+#print("Floating Number, gravity", 9.81)
+#
+##Complex number
+#print("Complex number: ", 1+1j)
+#print("Multiplying complex number: ", (1+1j)*(1-1j))
+
+
+##Declaring the variable at the top first
+#a=3
+#b=2
+##Arithmetical operations for these
+#total = a+b
+#diff = a-b
+#product = a*b
+#division = a/b
+#remainder = a%b
+#floor_division = a//b
+#exponential = a**b
+#
+#print("a+b= ", total)
+#print("a-b= ", diff)
+#print("a*b= ", product)
+#print("a/b= ", division)
+#print("a%b= ", floor_division)
+#print("a**b= ", exponential)
+
+
+##Calculating area of a circle
+#radius=10
+#area= 3.14*radius**2
+#print("Area of a circle of radius 10 is: ", area)
+#
+##Calculating area of a rectangle
+#length=10
+#width=20
+#rectangle=length*width
+#print("Area of a rectangle with length 10 and area 20 is : ", rectangle)
+#
+##Calculating a weight of an object
+#mass =75
+#gravity = 9.81
+#weight = mass*gravity
+#print("Weight of an object of mass 75 is : ", weight)
+
+#print(3>2)
+#print(3>=2)
+#print(3<2)
+#print(2<3)
+#print(2<=3)
+#print(3==2)
+#print(3!=2)
+#print(len('mango')== len('avocado'))
+#print(len('mango')!= len("avocado"))
+#print(len("mango")< len('avocado'))
+#print(len("milk")!= len("meat"))
+#print(len("tomato")== len("potato"))
+#print(len('python')> len("dragon"))
+
+#Boolean comparison
+print("True==True: ", True == True)
+print("True==False: ", True==False)
+print("True and True: ", True and True)
+print("True or Flase: ", True or False)
---
+++ learning_loopholes/project_learning.py
@@ -0,0 +1,9 @@
+#English explanantion of project
+#Step 1:- Ask user to type their name
+#Step 2:- Pick a random number by code
+#Step 3:- Print the name with bounty together
+
+import random
+a = input("ENTER YOUR NAME: ")
+bounty = random.randint(1,99999999)
+print(a,"Bounty: $", bounty) 
