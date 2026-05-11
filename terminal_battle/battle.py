b430d5a55a3a7e41204474c2d2ba960c520681aa
Dixit106 <dixitk2958@gmail.com>
2026-05-11 11:56:40 +0530

    boss logic and complete system for game! defeating boss is difficult :3

--- terminal_battle/battle.py
+++ terminal_battle/battle.py
@@ -17,18 +17,35 @@ print("Player Stats: ", "\nName:", player["Name"], "\nHP:", player["HP"], "\nPot
 
 while True:
     print("--- NEW TURN ---")
-    action = input("Attack OR Heal")
+    action = input("Attack OR Heal: ")
 
     if action == "Attack":
-        player_dmg = random.randint(15,30)
+        player_dmg = random.randint(15,40)
         boss["BossHP"] = boss["BossHP"] - player_dmg
         print("You hit", boss["NameBoss"], "with", [player_dmg],"damage!")
 
     elif action == "Heal":
-        player["HP"] = player["HP"] + 30
-        player["Potions"] = player["Potions"] - 1
-        print("You healed, +30 HP!")
-        print("Potions left: ", player["Potions"])
+        if player["Potions"] > 0:
+            player["HP"] = player["HP"] + 30
+            player["Potions"] = player["Potions"] - 1
+            print("You healed, +30 HP!")
+            print("Potions left: ", player["Potions"])
 
     else:
         print("Umm, That's not possible")
+
+
+    boss_dmg = random.randint(10,25)
+    player["HP"] = player["HP"] - boss_dmg
+    print("The boss only attacks and deals", [boss_dmg], "damage!")
+
+    print("Player Stats NEW: ", "\nName:", player["Name"], "\nHP:", player["HP"], "\nPotions", player["Potions"])
+    print("Boss Stats NEW: ", "\nName:", boss["NameBoss"], "\nHP:", boss["BossHP"])
+
+    if boss["BossHP"] <= 0:
+        print("You defeated the monster! Your Adventure Continues")
+        break
+
+    elif player["HP"] <= 0:
+        print("You Died, Game Over")
+        break
