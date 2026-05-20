0fe20cb64731e4c907fbd27d831eabc19e58edef
Dixit106 <dixitk2958@gmail.com>
2026-05-20 20:32:10 +0530

    fixing some more bugs script was continuing even after break i don't like intendations in pyhton

--- terminal_battle/battle.py
+++ terminal_battle/battle.py
@@ -428,8 +428,8 @@ def start_battle(player, boss):
             os.system("paplay sounds/badass_victory.mp3 &")
             slow_print(f"You defeated the monster! Your Adventure Continues")
             time.sleep(1)
-            sys.exit
-
+            break
+        
         elif player["HP"] <= 0:
             game_over_art = r"""
                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡀⠀
@@ -453,7 +453,7 @@ def start_battle(player, boss):
             os.system("paplay sounds/game_over.mp3 &")
             slow_print(f"You Died... Game Over...")
             time.sleep(1)
-            sys.exit
+            break
 
 kai_art = r"""
                                                      ,     .
@@ -786,8 +786,8 @@ if player["HP"] > 0:
     slow_print(f"HP = 100 and Potions = {player['Potions']} ")
     time.sleep(3)
 
-slow_print("---STAGE 2: THE GATEWAY---")
-start_battle(player, keeper)    
+    slow_print("---STAGE 2: THE GATEWAY---")
+    start_battle(player, keeper)    
 
 if player["HP"] > 0:
     time.sleep(3)
@@ -834,21 +834,22 @@ if player["HP"] > 0:
     slow_print(f"HP = 100 and Potions = {player['Potions']} ")
     time.sleep(3)
 
-#The Final Battle:-
-time.sleep(3)
-slow_print(f"You Entered Final Boss Chamber, you saw a lady standing in front of you")
-time.sleep(3)
-slow_print(f"You went silent, unable to move and confused. You finally asked where is my lover and who are you?")
-time.sleep(3)
-slow_print(f"The Lady replied: Everything is right in front of you! Your Lover, Your Enemy, Death... Everything")
-time.sleep(3)
-slow_print(f"You focused on her face and your body shivered? You asked: Was this all an act!")
-time.sleep(3)
-slow_print(f"She replied: So you have finally figured it out huh!. You humans have one big strenght and one big weakness... unfortunatly both of them are same. Your fellings.")
-time.sleep(3)
-slow_print(f"I have used this trick countless times on countless warriors who were said to be the strongest... but they were weak and thanks to that I still stand here using the same trick and will continue using it.")
-time.sleep(3)
-slow_print(f"You replied in Anger: this will be the last time!")
-time.sleep(3)
-slow_print(f"----The End Game----")
-start_battle(player, lover)
+    #The Final Battle:-
+    time.sleep(3)
+    slow_print(f"You Entered Final Boss Chamber, you saw a lady standing in front of you")
+    time.sleep(3)
+    slow_print(f"You went silent, unable to move and confused. You finally asked where is my lover and who are you?")
+    time.sleep(3)
+    slow_print(f"The Lady replied: Everything is right in front of you! Your Lover, Your Enemy, Death... Everything")
+    time.sleep(3)
+    slow_print(f"You focused on her face and your body shivered? You asked: Was this all an act!")
+    time.sleep(3)
+    slow_print(f"She replied: So you have finally figured it out huh!. You humans have one big strenght and one big weakness... unfortunatly both of them are same. Your fellings.")
+    time.sleep(3)
+    slow_print(f"I have used this trick countless times on countless warriors who were said to be the strongest... but they were weak and thanks to that I still stand here using the same trick and will continue using it.")
+    time.sleep(3)
+    slow_print(f"You replied in Anger: this will be the last time!")
+    time.sleep(3)
+    slow_print(f"----The End Game----")
+    start_battle(player, lover)
+    
